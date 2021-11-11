d7e6fb1986a9bddbc82c88108134ec6ea4843f52
Alexey Chernenko <alexey@nexgent.com>
2021-11-11 18:45:05 +0200

    chore: process batches in parallel

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const parser = require("csv-parse/lib/sync");
 const stringify = require("csv-stringify/lib/sync");
 
 const {
-  LINKED_IN_USERNAME,
-  LINKED_IN_PASSWORD,
+  LINKED_IN_ACCOUNTS,
   INPUT_FILE_PATH,
   OUTPUT_FILE_PATH,
   LINKEDIN_URL_CSV_COLUMN_NAME,
+  BATCH_SIZE,
 } = process.env;
 
 const URL_PROTOCOL_SUB_DOMAIN_REGEX = /^https:\/\/www\./gim;
@@ -16,6 +16,16 @@ const LINKEDIN_URL_PATH_REGEX = /linkedin\.com\/in\/.+/gim;
 
 const log = (...params) =>
   console.log(`[${new Date().toISOString()}]`, ...params);
+
+const chunk = (inputArray, perChunk) =>
+  inputArray.reduce((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index / perChunk);
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = []; // start a new chunk
+    }
+    resultArray[chunkIndex].push(item);
+    return resultArray;
+  }, []);
 
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -68,9 +78,9 @@ const login = async (page, username, password) => {
   log("logged in successfully");
 };
 
-const scrapePage = async (page, index, user) => {
+const scrapePage = async (page, index, user, threadId) => {
   const linkedInUrl = user[LINKEDIN_URL_CSV_COLUMN_NAME];
-  log(`[${index}] start processing`, linkedInUrl);
+  log(`[ThreadID=${threadId}][${index}] start processing`, linkedInUrl);
   await page.goto(linkedInUrl, {
     waitUntil: ["load", "domcontentloaded"],
   });
@@ -79,7 +89,7 @@ const scrapePage = async (page, index, user) => {
   await page.waitForTimeout(500);
   await page.evaluate("window.scrollBy(0,600)");
   const delayMs = getRandomInt(55000, 65000);
-  log("waiting", msToTime(delayMs));
+  log(`[ThreadID=${threadId}] waiting`, msToTime(delayMs));
   await page.waitForTimeout(delayMs);
   const jobs = await page.evaluate(() => {
     const experienceElements = [
@@ -143,7 +153,7 @@ const scrapePage = async (page, index, user) => {
     waitUntil: ["load", "domcontentloaded"],
   });
   await page.waitForTimeout(3000);
-  log(`[${index}] record processed`, linkedInUrl, "jobs found:", jobs.length);
+  log(`[ThreadID=${threadId}][${index}] record processed`, linkedInUrl, "jobs found:", jobs.length);
   return jobs.map((job) => ({
     ...user,
     "Date of Scrape": new Date().toISOString(),
@@ -176,7 +186,7 @@ const validate = (users) =>
       };
     });
 
-const scrapeUsingAccount = async (userName, password, users) => {
+const scrapeUsingAccount = async (userName, password, users, threadId) => {
   if (!users.length) {
     return;
   }
@@ -197,7 +207,7 @@ const scrapeUsingAccount = async (userName, password, users) => {
   await page.screenshot({ path: "login.png" });
 
   for (let i = 0; i < users.length; i++) {
-    records.push(...(await scrapePage(page, i, users[i])));
+    records.push(...(await scrapePage(page, i + 1, users[i], threadId)));
   }
 
   await browser.close();
@@ -206,11 +216,18 @@ const scrapeUsingAccount = async (userName, password, users) => {
 };
 
 const main = async () => {
-  if (!LINKED_IN_USERNAME) {
-    throw new Error("Please specify LINKED_IN_USERNAME in the env.local file.");
+  if (!BATCH_SIZE) {
+    throw new Error("Please specify BATCH_SIZE in the env.local file.");
   }
-  if (!LINKED_IN_PASSWORD) {
-    throw new Error("Please specify LINKED_IN_PASSWORD in the env.local file.");
+  const batchSize = Number(BATCH_SIZE);
+  if (!LINKED_IN_ACCOUNTS) {
+    throw new Error("Please specify LINKED_IN_ACCOUNTS in the env.local file.");
+  }
+  const accounts = JSON.parse(LINKED_IN_ACCOUNTS);
+  if (!accounts.length) {
+    throw new Error(
+      "Please specify at least one linkedIn account in JSON format. Use LINKED_IN_ACCOUNTS environment variable in the env.local file."
+    );
   }
   const users = validate(parseCsvFile(INPUT_FILE_PATH));
   if (!users.length) {
@@ -218,12 +235,21 @@ const main = async () => {
     return;
   }
   log("found user to process:", users.length);
+  if (Math.ceil(users.length / batchSize) > accounts.length) {
+    throw new Error(
+      `There's not enough linkedIn accounts specified to process ${users.length} users. Each linkedIn account can process up to ${BATCH_SIZE} profiles.`
+    );
+  }
 
-  const records = await scrapeUsingAccount(
-    LINKED_IN_USERNAME,
-    LINKED_IN_PASSWORD,
-    users
-  );
+  const batches = chunk(users, batchSize);
+  const records = (
+    await Promise.all(
+      batches.map((batch, batchIndex) => {
+        const { userName, password } = accounts[batchIndex];
+        return scrapeUsingAccount(userName, password, batch, batchIndex + 1);
+      })
+    )
+  ).flat();
 
   saveCsvFile(OUTPUT_FILE_PATH, records);
   log("the result has been saved to", OUTPUT_FILE_PATH, "file");
